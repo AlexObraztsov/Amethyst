b9546faa93ffc17204a2facb782ba0439cf074c3
Alexey Obraztsov <AlexeyObraztsov@coherentsolutions.com>
2018-12-05 18:04:27 +0300

    add some files

---
+++ Amethyst.Domain/Devices/BaseHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Amethyst.Domain.Devices
+{
+    public class BaseHub
+    {
+        private List<IBaseDevice> _devices;
+
+        public BaseHub()
+        {
+            _devices = new List<IBaseDevice>();
+        }
+
+        public void RegisterDevice(IBaseDevice device)
+        {
+            _devices.Add(device);
+        }
+    }
+}
---
+++ Amethyst.Domain/Devices/IBaseDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amethyst.Domain.Devices
+{
+    public interface IBaseDevice
+    {
+        Guid Id { get; set; }
+    }
+}
